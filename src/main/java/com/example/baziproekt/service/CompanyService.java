da267d8a8b667374525023355672baf3069055ff
Andrea <andrea.gadzhovska@students.finki.ukim.mk>
2024-05-20 12:28:52 +0200

    Fixed conflicts

--- src/main/java/com/example/baziproekt/service/CompanyService.java
+++ src/main/java/com/example/baziproekt/service/CompanyService.java
@@ -1,12 +1,6 @@
 package com.example.baziproekt.service;
-
-
-
 import com.example.baziproekt.model.Company;
-<<<<<<< HEAD
-=======
 import com.example.baziproekt.model.Rating;
->>>>>>> 3978ccb44ab00caa15afabd6223ad96abc64c9a5
 
 import java.util.List;
 
@@ -17,9 +11,6 @@ public interface CompanyService {
     Company create(String name, String email);
 
     Company findById(Integer id);
-<<<<<<< HEAD
-=======
 
     Company updateRatings(Integer id, List<Rating> ratings);
->>>>>>> 3978ccb44ab00caa15afabd6223ad96abc64c9a5
 }
